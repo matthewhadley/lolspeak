11fdff39513daa350f42777563bbb08fd6450f18
matthewhadley <matthew.hadley@gmail.com>
2018-09-02 13:03:15 -0700

    update standards

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ var YAML = require('yamljs');
 var path = require('path');
 var dict = YAML.load(path.join(__dirname, 'tranzlator.yml'));
 
-function translate(phrase) {
-  var words = phrase.toUpperCase().split(/(?: |,|\.|\!)+/);
+function translate (phrase) {
+  var words = phrase.toUpperCase().split(/(?: |,|\.|!)+/);
   var translation = '';
 
-  words.forEach(function(word) {
+  words.forEach(function (word) {
     if (dict[word]) {
       word = dict[word];
     }
@@ -17,12 +17,12 @@ function translate(phrase) {
   return translation.trim();
 }
 
-module.exports = function(phrase) {
+module.exports = function (phrase) {
   var translation = '';
   var lines = phrase.split(/\n/);
   var ln = lines.length;
 
-  lines.forEach(function(line) {
+  lines.forEach(function (line) {
     translation = translation + translate(line);
     if (ln > 1) {
       translation = translation + '\n';
